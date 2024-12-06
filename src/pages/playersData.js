becd2547d8e37724a955ee26f13884060b8e2e86
ludemo <azevallosa@unsa.edu.pe>
2024-12-05 23:34:22 -0500

    puestos actualizados

--- src/pages/playersData.js
+++ src/pages/playersData.js
@@ -1,86 +1,84 @@
 export const jugadores = [
   {
+    name: 'Robert Rodolfo',
+    role: 'Backend',
+    xp: 110, // +25 puntos
+    progress: 20,
+    badges: [
+      { icon: '✅', description: 'Finalizador experto' },
+    ],
+    rank: 1, // Nuevo ranking
+    nivel: 18,
+  },
+  {
     name: 'Lucely Rubi',
     role: 'Frontend',
-    xp: 90,
-    progress: 0,
+    xp: 105, // +15 puntos
+    progress: 50,
     badges: [
       { icon: '🏋️', description: "Pilar del Sprint" },
     ],
-    rank: 1,
-    nivel: 18,
-  },
-  {
-    name: 'Robert Rodolfo',
-    role: 'Backend',
-    xp: 85,
-    progress: 0,
-    badges: [
-      { icon: '✅', description: 'Finalizador experto' }
-    ],
-    rank: 2,
-    nivel: 18,
-  },
-  {
-    name: 'Rommel',
-    role: 'Frontend',
-    xp: 60,
-    progress: 0,
-    badges: [
-    ],
-    rank: 3,
-    nivel: 18,
-  },
-  {
-    name: 'Jhon Franklin',
-    role: 'Backend',
-    xp: 55,
-    progress: 0,
-    badges: [
-      { icon: '✅', description: 'Finalizador experto' }
-    ],
-    rank: 4,
+    rank: 2, // Nuevo ranking
     nivel: 18,
   },
   {
     name: 'Anthony Juancarlo',
     role: 'Backend',
-    xp: 50,
-    progress: 0,
+    xp: 75, // +25 puntos
+    progress: 66,
     badges: [
+      { icon: '🤝', description: 'Colaborador destacado' }, // Logro añadido
     ],
-    rank: 5,
+    rank: 3, // Nuevo ranking
+    nivel: 18,
+  },
+  {
+    name: 'Jhon Franklin',
+    role: 'Backend',
+    xp: 70, // +15 puntos
+    progress: 33,
+    badges: [
+      { icon: '✅', description: 'Finalizador experto' },
+    ],
+    rank: 4, // Nuevo ranking
+    nivel: 18,
+  },
+  {
+    name: 'Rommel',
+    role: 'Frontend',
+    xp: 70, // +10 puntos
+    progress: 50,
+    badges: [],
+    rank: 5, // Nuevo ranking
     nivel: 18,
   },
   {
     name: 'Alex Antonio',
     role: 'Frontend',
-    xp: 30,
+    xp: 40, // +10 puntos
     progress: 0,
-    badges: [
-    ],
-    rank: 6,
+    badges: [],
+    rank: 6, // Nuevo ranking
     nivel: 10,
   },
   {
     name: 'Glenny Shinderly',
     role: 'Frontend',
-    xp: 10,
-    progress: 0,
-    badges: [
-    ],
-    rank: 7,
+    xp: 20, // +10 puntos
+    progress: 33,
+    badges: [],
+    rank: 7, // Nuevo ranking
     nivel: 15,
   },
   {
     name: 'Maria Margareth',
     role: 'Lider de proyecto / Tester',
-    xp: 10,
+    xp: 10, // Sin cambios
     progress: 0,
     badges: [
       { icon: '🫂', description: 'Más participativo' },
     ],
-    rank: 8,
+    rank: 8, // Sin cambios
     nivel: 15,
   },
 ];
