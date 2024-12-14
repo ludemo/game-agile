1a22a4ac15192230b83b3d7aa51c5f6fb1579e07
ludemo <azevallosa@unsa.edu.pe>
2024-12-13 21:25:10 -0500

    puestos actualizados

--- src/pages/playersData.js
+++ src/pages/playersData.js
@@ -2,7 +2,7 @@ export const jugadores = [
   {
     name: 'Anthony Juancarlo',
     role: 'Backend',
-    xp: 150, // +10 XP por reconocimiento
+    xp: 180, // Sin cambios
     progress: 66,
     badges: [
       { icon: '🤝', description: 'Colaborador destacado' },
@@ -11,69 +11,69 @@ export const jugadores = [
     nivel: 18,
   },
   {
+    name: 'Lucely Rubi',
+    role: 'Frontend',
+    xp: 180, // +10 XP
+    progress: 100,
+    badges: [
+      { icon: '🏋️', description: "Pilar del Sprint" },
+      { icon: '⭐', description: "Finalizadora Estrella" },
+    ],
+    rank: 1, // Empate con Anthony
+    nivel: 18,
+  },
+  {
     name: 'Jhon Franklin',
     role: 'Backend',
-    xp: 145, // +10 XP por reconocimiento
+    xp: 155, // Sin cambios
     progress: 33,
     badges: [
       { icon: '✅', description: 'Finalizador experto' },
-      { icon: '🌙', description: 'Noctámbulo del Sprint' }, 
+      { icon: '🌙', description: 'Noctámbulo del Sprint' },
     ],
-    rank: 3, // Segundo en XP
+    rank: 3, // Tercero en XP
     nivel: 18,
   },
   {
     name: 'Robert Rodolfo',
     role: 'Backend',
-    xp: 140, // +10 XP por reconocimiento
+    xp: 150, // Sin cambios
     progress: 75,
     badges: [
       { icon: '✅', description: 'Finalizador experto' },
       { icon: '🏋️', description: 'Pilar del Sprint' },
     ],
-    rank: 4, // Tercero en XP
-    nivel: 18,
-  },
-  {
-    name: 'Lucely Rubi',
-    role: 'Frontend',
-    xp: 150, // +10 XP por reconocimiento
-    progress: 100,
-    badges: [
-      { icon: '🏋️', description: "Pilar del Sprint" },
-      { icon: '⭐', description:  "Finalizadora Estrella"}
-    ],
-    rank: 1, // Tercero en XP (empatado con Robert)
-    nivel: 18,
-  },
-  {
-    name: 'Rommel',
-    role: 'Frontend',
-    xp: 95, // +10 XP por reconocimiento
-    progress: 50,
-    badges: [
-      { icon: '✅', description: 'Finalizador experto' },
-    ],
-    rank: 5, // Quinto en XP
+    rank: 4, // Cuarto en XP
     nivel: 18,
   },
   {
     name: 'Maria Margareth',
     role: 'Lider de proyecto / Tester',
-    xp: 90, // +15 XP por reconocer a todos
+    xp: 130, // +15 XP
     progress: 77,
     badges: [
       { icon: '🫂', description: 'Más participativo' },
       { icon: '⭐', description: 'Líder consistente' },
       { icon: '🌟', description: 'Inspiradora del Sprint' },
     ],
-    rank: 6, // Sexto en XP
+    rank: 5, // Quinto en XP
     nivel: 15,
+  },
+  {
+    name: 'Rommel',
+    role: 'Frontend',
+    xp: 115, // +10 XP
+    progress: 50,
+    badges: [
+      { icon: '✅', description: 'Finalizador experto' },
+    ],
+    rank: 6, // Sexto en XP
+    nivel: 18,
   },
   {
     name: 'Alex Antonio',
     role: 'Frontend',
-    xp: 85, // +10 XP por reconocimiento
+    xp: 95, // Sin cambios
     progress: 0,
     badges: [],
     rank: 7, // Séptimo en XP
@@ -82,10 +82,10 @@ export const jugadores = [
   {
     name: 'Glenny Shinderly',
     role: 'Frontend',
-    xp: 76, // +10 XP por reconocimiento
+    xp: 96, // +10 XP
     progress: 66,
     badges: [],
-    rank: 8, // Último en XP
+    rank: 7, // Empata con Alex Antonio en XP
     nivel: 15,
   },
 ];
